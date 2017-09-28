6145d2b1a6fa5c01df966c74ff1069b1b2061bac
Hao <lehuongchu@gmail.com>
2017-09-28 20:19:32 +0700

    foodTracker lam bang coreData ban hoan thien

---
+++ FoodTracker(CoreData)/MealTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker(CoreData)
+//
+//  Created by Hao on 9/28/17.
+//  Copyright © 2017 Hao. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ FoodTracker(CoreData)Tests/FoodTracker_CoreData_Tests.swift
@@ -0,0 +1,15 @@
+//
+//  FoodTracker_CoreData_Tests.swift
+//  FoodTracker(CoreData)Tests
+//
+//  Created by Hao on 9/28/17.
+//  Copyright © 2017 Hao. All rights reserved.
+//
+
+import XCTest
+@testable import FoodTracker_CoreData_
+
+class FoodTrackerTests: XCTestCase {
+    
+
+}
